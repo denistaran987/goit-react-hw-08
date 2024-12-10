f5f5d123043f1c603c971176432e660fa9dffbcb
Denis <denistaran482@gmail.com>
2024-12-10 19:30:13 +0200

    some fix

--- src/components/Layout/UserMenu/UserMenu.jsx
+++ src/components/Layout/UserMenu/UserMenu.jsx
@@ -1,10 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from '../../../redux/auth/selectors';
+import { useDispatch } from 'react-redux';
 import { logout } from '../../../redux/auth/operations';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
-  const user = useSelector(selectUser);
+  // const user = useSelector(selectUser);
   return (
     <div>
       {/* <p>Welcome, ${user.name}</p> */}
